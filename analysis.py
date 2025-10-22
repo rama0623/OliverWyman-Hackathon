4f5779a81721f0d7b197b5ed5cbc9669ed17c192
Zak Paswal <103907155+zibp@users.noreply.github.com>
2025-10-22 15:24:26 +0100

    updated analysis.py
    adds linear regression model

--- analysis.py
+++ analysis.py
@@ -13,3 +13,22 @@ vo2 = csv_file["VO2 Max"]
 bmi = csv_file["BMI"]
 marathons = csv_file["n Marathons run"]
 cadence = csv_file["Cadence"]
+
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy import stats
+
+xpoints = np.array( train10.to_numpy() )
+
+ypoints = np.array( cadence.to_numpy() )
+
+slope, intercept, r, p, std_error = stats.linregress(xpoints, ypoints)
+
+def linearRegression(x):
+  return slope * x + intercept
+
+model = list(map(linearRegression, xpoints))
+
+plt.scatter(xpoints, ypoints)
+plt.plot(xpoints, model)
+plt.show()
